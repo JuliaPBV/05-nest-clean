23244182adb22ff7ed0b32714e557a7558b8799a
Julia Pereira <vital.pereira.julia@gmail.com>
2024-12-24 08:37:36 -0300

    Comunicação entre camadas

--- src/domain/notification/application/repositories/questions-repository.ts
+++ src/domain/notification/application/repositories/questions-repository.ts
@@ -1,11 +1,11 @@
 import { PaginationsParams } from '@/core/repositories/pagination-params'
 import { Question } from '@prisma/client'
 
-export interface QuestionsRepository {
-  findById(id: string): Promise<Question | null>
-  findBySlug(slug: string): Promise<Question | null>
-  findManyRecent(params: PaginationsParams): Promise<Question[]>
-  save(question: Question): Promise<void>
-  create(question: Question): Promise<void>
-  delete(question: Question): Promise<void>
+export abstract class QuestionsRepository {
+  abstract findById(id: string): Promise<Question | null>
+  abstract findBySlug(slug: string): Promise<Question | null>
+  abstract findManyRecent(params: PaginationsParams): Promise<Question[]>
+  abstract save(question: Question): Promise<void>
+  abstract create(question: Question): Promise<void>
+  abstract delete(question: Question): Promise<void>
 }
